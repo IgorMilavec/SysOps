6eb59a9e0973b4e5a8f2055b2710ef42c1f505e7
Igor Milavec <igor.milavec@gmail.com>
2019-06-06 14:24:34 +0200

    Create Backup status.sql

---
+++ Microsoft SQL/Backup status.sql
@@ -0,0 +1,22 @@
+SELECT  name DatabaseName, 
+             recovery_model_desc RecoveryModel,
+             logstats.LogUsageMB,
+             log_reuse_wait_desc LogReuseWait,
+             LastFullBackup, LastLogBackup
+FROM    sys.databases
+             LEFT JOIN (
+                    SELECT backupset.database_name DatabaseName, 
+                           MAX(CASE WHEN backupset.type = 'D' THEN backupset.backup_finish_date ELSE NULL END) AS LastFullBackup,
+                           MAX(CASE WHEN backupset.type = 'I' THEN backupset.backup_finish_date ELSE NULL END) AS LastDifferentialBackup,
+                           MAX(CASE WHEN backupset.type = 'L' THEN backupset.backup_finish_date ELSE NULL END) AS LastLogBackup
+                    FROM    msdb.dbo.backupset
+                    GROUP BY backupset.database_name
+             ) backupstats ON (name = backupstats.DatabaseName)
+             LEFT JOIN (
+                    SELECT  RTRIM(instance_name) [DatabaseName], 
+                                  cntr_value / 1024 LogUsageMB
+                    FROM   sys.dm_os_performance_counters 
+                    WHERE  object_name = 'SQLServer:Databases'
+                           AND counter_name = 'Log File(s) Used Size (KB)'
+                           AND instance_name <> '_Total'
+             ) logstats ON (name = logstats.DatabaseName)
