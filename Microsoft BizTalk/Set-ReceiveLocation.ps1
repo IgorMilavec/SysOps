be9a7cac1fe9a922ec0340da488e01e5de2b4e40
Igor Milavec <igor.milavec@gmail.com>
2020-11-14 09:28:28 +0100

    Update Set-ReceiveLocation.ps1

--- Microsoft BizTalk/Set-ReceiveLocation.ps1
+++ Microsoft BizTalk/Set-ReceiveLocation.ps1
@@ -4,7 +4,7 @@
  Use the Set-ReceiveLocation cmdlet to modify the settings of existing BizTalk receive locations.
 
 .EXAMPLE
- powershell -ExecutionPolicy ByPass -Noprofile -Noninteractive -Command .\Set-ReceiveLocation.ps1 'Location name' -Enabled $True
+ powershell -ExecutionPolicy ByPass -Noprofile -Noninteractive -Command .\Set-ReceiveLocation.ps1 'Location name' -Status Enabled
 #>
 
 param(
@@ -12,23 +12,26 @@ param(
 	[string]$Name, 
 
 	[Parameter(Mandatory=$false)]
-	[bool]$Enabled
+	[string]$Status
 )
 Set-StrictMode -Version 2.0
 
 $receiveLocation = get-wmiobject msbts_receivelocation -Namespace 'root\MicrosoftBizTalkServer' -Filter "name='$Name'"
 if ($receiveLocation -eq $null)
 {
-	throw "The operation couldn't be performed because receive location '$Name' couldn't be found."
+	Write-Warning "The operation couldn't be performed because receive location '$Name' couldn't be found."
 }
 
-if ($PSBoundParameters.ContainsKey("Enabled")){
-	if ($Enabled)
-	{
-		$receiveLocation.Enable() | Out-Null
-	}
-	else
-	{
-		$receiveLocation.Disable() | Out-Null
+if ($PSBoundParameters.ContainsKey("Status")) {
+	switch ($Status) {
+		"Enabled" {
+			$receiveLocation.Enable() | Out-Null
+		}
+		"Disabled" {
+			$receiveLocation.Disable() | Out-Null
+		}
+		default {
+			Write-Warning "The operation couldn't be performed because receive location status '$Status' isn't supported."
+		}
 	}
 }
