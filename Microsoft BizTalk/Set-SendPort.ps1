400dca00c8bbe22f7824dc1b258f4ce8f8aafd9f
Igor Milavec <igor.milavec@gmail.com>
2021-05-08 00:38:00 +0200

    Add status output

--- Microsoft BizTalk/Set-SendPort.ps1
+++ Microsoft BizTalk/Set-SendPort.ps1
@@ -26,9 +26,11 @@ if ($PSBoundParameters.ContainsKey("Status")) {
 	switch ($Status) {
 		"Started" {
 			$sendPort.Start() | Out-Null
+			Write-Host "Send port '$Name' has been started."
 		}
 		"Stopped" {
 			$sendPort.Stop() | Out-Null
+			Write-Host "Send port '$Name' has been stopped."
 		}
 		default {
 			Write-Warning "The operation couldn't be performed because send port status '$Status' isn't supported."
